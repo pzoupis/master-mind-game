7e7229a11532c5c53a037a3264057489283e21af
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2018-07-08 22:37:28 +0300

    Add logging with log4j2

--- src/main/java/me/pzoupis/mastermind/Main.java
+++ src/main/java/me/pzoupis/mastermind/Main.java
@@ -1,10 +1,14 @@
 package me.pzoupis.mastermind;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import me.pzoupis.mastermind.mastermindgame.Game;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     
     public static void main(String[] args) {
+        logger.info("The game begins");
         Game game = new Game();
         game.playGame();
     }
--- src/main/java/me/pzoupis/mastermind/domain/CodeMaker.java
+++ src/main/java/me/pzoupis/mastermind/domain/CodeMaker.java
@@ -1,14 +1,21 @@
 package me.pzoupis.mastermind.domain;
 
+import java.util.Arrays;
 import me.pzoupis.mastermind.interfaces.ICodeMaker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CodeMaker implements ICodeMaker {
     
+    private static final Logger logger = LogManager.getLogger(CodeMaker.class);
+    
+    @Override
     public int[] generateCode() {
         int[] code = new int[4];
         for(int i = 0; i < 4; i++) {
             code[i] = getRandomDigit();
         }
+        logger.debug("The generated code is: " + Arrays.toString(code));
         return code;
     }
     
